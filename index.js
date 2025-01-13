768cb195d8c390717afec52f38f2165edcf4c309
p <philipp.norton@posteo.de>
2025-01-13 12:41:37 +0100

    IT WORKS

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const port = 3000;
 
+
+// Set up the CORS middleware
+app.use(cors({}));
+
+
+
 const proxyNewcastleUO = createProxyMiddleware({
-  target: 'https://newcastle.urbanobservatory.ac.uk/api/v1.1',
-  changeOrigin: true, // needed for virtual hosted sites
+  target: 'https://newcastle.urbanobservatory.ac.uk',
+  changeOrigin: true,
+  headers: {
+    // Origin: 'http://localhost:5173',
+    // 'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    // 'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+  },
+  logger: console,
 });
 
 app.use('/api/nuo', proxyNewcastleUO);

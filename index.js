2f34c143913a2f7d5d9b7e868e920155b2d33978
p <philipp.norton@posteo.de>
2025-01-13 11:49:27 +0100

    fix typos, add middleware to app.use()

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const { createProxyMiddelware } = require('http-proxy-middelware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const port = 3000;
 
-const proxyNewcastleUO = createProxyMiddelware({
+const proxyNewcastleUO = createProxyMiddleware({
   target: 'https://newcastle.urbanobservatory.ac.uk/api/v1.1',
   changeOrigin: true, // needed for virtual hosted sites
 });
 
-app.use('/api/nuo');
+app.use('/api/nuo', proxyNewcastleUO);
 
 app.listen(port, () => {
   console.log('generest server listening on port ' + port);
